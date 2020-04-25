0cc8bafd1adb1ed557bb99c7b114d27637dc4326
azexs <mtarka1337@gmail.com>
2020-04-26 00:18:22 +0200

    Wybiera event

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -2,6 +2,7 @@
 # testertest
 from time import sleep
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 
 
 class Bot:
@@ -16,7 +17,33 @@ class Bot:
         self.driver.find_element_by_xpath("//*[@id=\"form-email\"]").send_keys(email)
         self.driver.find_element_by_xpath("//*[@id=\"form-password\"]").send_keys(password)
         sleep(2)
-        self.driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/form/fieldset/button/span").click()
+
+        #self.driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/form/fieldset/button/span").click()
+        #self.driver.find_element_by_class_name('sc-bxivhb kMEQZP')
+        self.driver.find_element_by_tag_name('button').click()
+
+        sleep(3)
+
+        try:
+            campaign_id = 'campaign-ZZO119M'
+            action = ActionChains(self.driver)
+            first_compaing = self.driver.find_element_by_xpath('//*[@id="'+campaign_id+'"]/div')
+            action.move_to_element(first_compaing).perform()
+            second_compaing = self.driver.find_element_by_xpath('//*[@id="'+campaign_id+'"]/div/div[1]/div/button/span')
+            action.move_to_element(second_compaing).perform()
+            second_compaing.click()
+        except Exception as e:
+            print('błąd '+ str(e))
+            self.driver.quit
+
+
+        # size_select = 'M'
+        # click_size = self.driver.find_element_by_xpath('//*[@id="inner-wrapper"]/section/div[2]/nav/a[2]/span')
+        # if size_select != '':
+        #     click_size.click()
+        #     sleep(1)
+        #     set_size = self.driver.find_element_by_xpath('//*[@id="'+size_select+'"]').click()
+        #     sleep(1)
 
 
 Bot("piotrpopisgames@gmail.com", 'testertest')
