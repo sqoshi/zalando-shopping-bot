679d4eecf64ba9930c28afb7c6a688d75a91f02e
Piotr <piotrpopis98@icloud.com>
2020-04-26 14:55:53 +0200

    logging

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -2,21 +2,81 @@
 # testertest
 from time import sleep
 from selenium import webdriver
+import requests
+from bs4 import BeautifulSoup
+from selenium.common.exceptions import StaleElementReferenceException
 
 
-class Bot:
+class ShoppingBot:
     def __init__(self, email, password):
         self.driver = webdriver.Firefox()
+        # Open website
         self.driver.get("https://www.zalando-lounge.pl")
-        sleep(3)
-        self.driver.find_element_by_xpath(
-            "/html/body/div[2]/div/div[2]/div[1]/div/div/div[1]/div/div/div[2]/div/div/button").click()
-        self.driver.find_element_by_xpath(
-            "//*[@id=\"uc-btn-accept-banner\"]").click()
-        self.driver.find_element_by_xpath("//*[@id=\"form-email\"]").send_keys(email)
-        self.driver.find_element_by_xpath("//*[@id=\"form-password\"]").send_keys(password)
-        sleep(2)
-        self.driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/form/fieldset/button/span").click()
+        while 1:
+            try:
+                self.driver.get("https://www.zalando-lounge.pl")
+                break
+            except:
+                print("Retrying !!!")
+        # Open loggin panel
+        while 1:
+            try:
+                self.driver.find_element_by_xpath(
+                    "/html/body/div[2]/div/div[2]/div[1]/div/div/div[1]/div/div/div[2]/div/div/button").click()
+                break
+            except:
+                print("Retrying !!!")
+
+        # Off annoying banner
+        while 1:
+            try:
+                self.driver.find_element_by_xpath(
+                    "//*[@id=\"uc-btn-accept-banner\"]").click()
+                break
+            except:
+                print("Retrying !!!")
+
+            # Log into shop.
+        try:
+            self.driver.find_element_by_xpath("//*[@id=\"form-email\"]").send_keys(email)
+            self.driver.find_element_by_xpath("//*[@id=\"form-password\"]").send_keys(password)
+            self.driver.find_element_by_xpath(
+                "/html/body/div[1]/div/div[2]/div[2]/div/div/div/form/button").click()
+        except:
+            print("cant find login button !!!")
+            self.driver.find_element_by_xpath("//*[@id=\"form-email\"]").send_keys("")
+            self.driver.find_element_by_xpath("//*[@id=\"form-password\"]").send_keys("")
+
+        # select Man
+        while 1:
+            try:
+                sleep(3)
+                self.driver.find_element_by_xpath(
+                    "/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[2]/div/div/ul/li[3]/span").click()
+                break
+            except:
+                print("cant find Man")
+
+        elems = self.driver.find_elements_by_xpath("//a[@href]")
+        while 1:
+            try:
+                for elem in elems:
+                    # if "Puma" in elem.get_attribute("text") and "Rek" not in elem.get_attribute("text"):
+                    href = elem.get_attribute("href")
+                    text = elem.get_attribute("text")
+                    alt = elem.get_attribute("alt")
+                    clas = elem.get_attribute("class")
+                    print(href, text, alt, clas)
+                print("111111111111111111111111111111111111111111111111111111111111111111111111111111111111111")
+                raise Exception
+            except Exception as ex:
+                break
+        self.driver.get("https://www.zalando-lounge.pl/campaigns/ZZO10ZG/gender_134")
+        # elem = self.driver.find_element_by_xpath("//*")
+        # source_code = elem.get_attribute("innerHTML")
+        # filename = open('zalando.html', 'w')
+        # filename.write(source_code)
+        # filename.close()
 
 
-Bot("piotrpopisgames@gmail.com", 'testertest')
+ShoppingBot("piotrpopisgames@gmail.com", 'testertest')
