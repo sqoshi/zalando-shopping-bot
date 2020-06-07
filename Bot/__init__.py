b6a5529f5c332b780e53dd4c2f7d0ee3f2b3cf84
Piotr <piotrpopis98@icloud.com>
2020-06-07 13:53:00 +0200

    clickable button info link to readme

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -1,5 +1,3 @@
-# login info.shopping.bot@gmail.com
-# shopping123bot
 import smtplib
 import sys
 from time import sleep
@@ -17,11 +15,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 def sendMail(to, file):
     """
-    Function to inform user about founded products by e-mail.
-    :param to:
-    :param file:
-    :return:
-    """
+   Function to inform user about founded products by e-mail.
+   :param to:
+   :param file:
+   :return:
+   """
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
     server.login('info.shopping.bot@gmail.com', 'shopping123bot')
@@ -29,13 +27,14 @@ def sendMail(to, file):
     body = (open(file, "r").read())
     message = ("From: %s\r\n" % from_mail + "To: %s\r\n" % to + "Subject: %s\r\n" % '' + "\r\n" + body)
     server.sendmail(from_mail, to, message)
+    print('\'' + body + '\' to', to)
 
 
 class ShoppingBot:
     def __init__(self, acc, cats, sizs, brds, cid, mpi, maa, mail, is_mail_checked, ite):
         options = Options()
         # options.add_argument("--disable-notifications")
-        print(acc, cats, sizs, brds, cid, mpi, maa, mail, is_mail_checked, ite)
+        # print(acc, cats, sizs, brds, cid, mpi, maa, mail, is_mail_checked, ite)
         self.driver = webdriver.Firefox(options=options)
         self.inform_email = None
         if is_mail_checked:
@@ -53,11 +52,11 @@ class ShoppingBot:
 
     def scroll_shim(self, obj):
         """
-        Scrolling down to needed event
+       Scrolling down to needed event
 
-        :param obj:
-        :return:
-        """
+       :param obj:
+       :return:
+       """
         x = obj.location['x']
         y = obj.location['y']
         scroll_by_coord = 'window.scrollTo(%s,%s);' % (
@@ -70,9 +69,9 @@ class ShoppingBot:
 
     def scroll_down(self):
         """
-        Scrolling down all items on list
-        :return:
-        """
+       Scrolling down all items on list
+       :return:
+       """
         # Get scroll height.
         last_height = self.driver.execute_script("return document.body.scrollHeight")
         while True:
@@ -88,9 +87,9 @@ class ShoppingBot:
 
     def turn_off_banner(self):
         """
-        Turn off cookie acceptance banner
-        :return:
-        """
+       Turn off cookie acceptance banner
+       :return:
+       """
         try:
             self.driver.find_element_by_xpath(
                 "//*[@id=\"uc-btn-accept-banner\"]").click()
@@ -99,10 +98,10 @@ class ShoppingBot:
 
     def set_max_per_item(self, max_cost_per_item):
         """
-        Setting max price per item in filtering
-        :param max_cost_per_item:
-        :return:
-        """
+       Setting max price per item in filtering
+       :param max_cost_per_item:
+       :return:
+       """
 
         WebDriverWait(self.driver, 5).until(ec.element_to_be_clickable((By.XPATH, '//*[@id="price-max"]'))).click()
         if max_cost_per_item != 0:
@@ -111,10 +110,10 @@ class ShoppingBot:
 
     def set_brands(self, wanted_brands):
         """
-        Choosing brand in filtering as on the list.
-        :param wanted_brands:
-        :return:
-        """
+       Choosing brand in filtering as on the list.
+       :param wanted_brands:
+       :return:
+       """
         i = 1
         already_selected = []
         while i:
@@ -136,10 +135,10 @@ class ShoppingBot:
 
     def set_sizes(self, wanted_sizes):
         """
-        Setting sizes in filtering as in the given list.
-        :param wanted_sizes:
-        :return:
-        """
+       Setting sizes in filtering as in the given list.
+       :param wanted_sizes:
+       :return:
+       """
         i = 1
         already_selected = []
         while i:
@@ -158,9 +157,9 @@ class ShoppingBot:
 
     def wait_for_popup(self):
         """
-        Waits for popup and tries to close it.
-        :return:
-        """
+       Waits for popup and tries to close it.
+       :return:
+       """
         try:
             WebDriverWait(self.driver, 1).until(
                 ec.presence_of_element_located((By.XPATH, '//div[contains(@class,"sizeOverlayDialog")]')))
@@ -178,11 +177,11 @@ class ShoppingBot:
 
     def wait_for_atcButton(self, attempt, size):
         """
-        Waiting till animation of adding item to shopping cart is finished or leaves error.
-        :param attempt:
-        :param size:
-        :return:
-        """
+       Waiting till animation of adding item to shopping cart is finished or leaves error.
+       :param attempt:
+       :param size:
+       :return:
+       """
         try:
             WebDriverWait(self.driver, 2).until(ec.presence_of_element_located(
                 (By.XPATH, '//div[contains(@class, "animation-ball") and starts-with(@style, "transf") ]')))
@@ -192,7 +191,8 @@ class ShoppingBot:
             return True
 
         except TimeoutException:
-            print('Attempt: ', attempt)
+            pass
+            # print('Attempt: ', attempt)
 
             if attempt == 5:
                 return False
@@ -205,7 +205,7 @@ class ShoppingBot:
             # WebDriverWait(self.driver, 5).until(
             #     ec.element_to_be_clickable((By.XPATH, '//*[@id="addToCartButton"]'))).click()
 
-            button =  WebDriverWait(self.driver, 5).until(
+            button = WebDriverWait(self.driver, 5).until(
                 ec.element_to_be_clickable((By.XPATH, '//*[@id="addToCartButton"]/div[1]/div[1]')))
             self.driver.execute_script("arguments[0].click();", button)
 
@@ -217,30 +217,29 @@ class ShoppingBot:
 
     def wait_login_error(self):
         """
-        Tries to click log_in button after error in loging
-        :return:
-        """
+       Tries to click log_in button after error in loging
+       :return:
+       """
         sleep(2)
-
         if len(self.driver.find_elements_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div/div/div/form/button')) != 0:
             self.driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div/div/div/form/button').click()
             self.wait_login_error()
 
     def change_acc(self, href, size):
         """
-        Changing account after reaching max shopping cart
-        :param href:
-        :param size:
-        :return:
-        """
+       Changing account after reaching max shopping cart
+       :param href:
+       :param size:
+       :return:
+       """
         self.iteration += 1
         if self.iteration == len(self.accounts_list):
             return True
-
         WebDriverWait(self.driver, 5).until(
             ec.element_to_be_clickable((By.XPATH, '//span[text() = "Konto"]'))).click()
         WebDriverWait(self.driver, 5).until(
             ec.element_to_be_clickable((By.XPATH, '//span[contains(text(), "Wyloguj")]'))).click()
+        sleep(2)
         WebDriverWait(self.driver, 5).until(
             ec.element_to_be_clickable((By.XPATH, '//span[contains(text(), "Zaloguj")]'))).click()
 
@@ -259,14 +258,13 @@ class ShoppingBot:
 
         WebDriverWait(self.driver, 5).until(ec.element_to_be_clickable(
             (By.XPATH, '//span[contains(@class, "Size") and text()="' + size + '"]'))).click()
-
         return False
 
     def filter_event(self):
         """
-        Filtering event in filters( categories, sizes , maxprice...)
-        :return:
-        """
+       Filtering event in filters( categories, sizes , maxprice...)
+       :return:
+       """
         WebDriverWait(self.driver, 20).until(
             ec.presence_of_element_located((By.XPATH, '//div[starts-with(@class, "filters")]')))
         i = 1
@@ -292,9 +290,9 @@ class ShoppingBot:
 
     def perform_login(self):
         """
-        Logging to account, checks error, sends login,pwd
-        :return:
-        """
+       Logging to account, checks error, sends login,pwd
+       :return:
+       """
         # Wait for cookies banner and close it
         while True:
             try:
@@ -318,10 +316,10 @@ class ShoppingBot:
 
     def get_filtered_hrefs(self, ind=29):
         """
-        Gets all items as hrefs and filtering them with categories(given)
-        :param ind:
-        :return:
-        """
+       Gets all items as hrefs and filtering them with categories(given)
+       :param ind:
+       :return:
+       """
         hrefs = []
         all_items = self.driver.find_elements_by_xpath("//div[starts-with(@id, 'article-')]/a")
         for item in all_items:
@@ -338,9 +336,9 @@ class ShoppingBot:
 
     def dont_know_whats_doing_this_part(self):
         """
-        TODO CHANGE NAME OF THIS FUNCTION and comment whats it doing MICHAL TARECZKA
-        :return:
-        """
+       TODO CHANGE NAME OF THIS FUNCTION and comment whats it doing MICHAL TARECZKA
+       :return:
+       """
         self.campaign_id = 'campaign-' + self.campaign_id
         action = ActionChains(self.driver)
         first_campaign = WebDriverWait(self.driver, 120).until(
@@ -355,12 +353,12 @@ class ShoppingBot:
 
     def iterate_over_items(self, hrefs, selected_sizes, total_items=0):
         """
-        Went trough filtered hrefs and adding items to shopping cart
-        :param hrefs:
-        :param selected_sizes:
-        :param total_items:
-        :return:
-        """
+       Went trough filtered hrefs and adding items to shopping cart
+       :param hrefs:
+       :param selected_sizes:
+       :param total_items:
+       :return:
+       """
         for href in hrefs:
             self.driver.get(href)
 
@@ -405,21 +403,15 @@ class ShoppingBot:
                                 total_items = 0
                                 selected = 0
                                 if self.change_acc(href, size):
-                                    print('KONIEC')
-                                    # TODO WYSYLANIE MAILI ! na koniec \/
-                                    # if self.inform_email is not None:
-                                    #   sendMail(self.inform_email, 'messages/normally_finished')
+                                    if self.inform_email is not None:
+                                        sendMail(self.inform_email, 'messages/normally_finished')
                                     return  # Koniec dodawania
-                               
 
-#ShoppingBot(["piotrpopisgames@gmail.com testertest", "mtarka1337@gmail.com Azexs1998"], ['koszula'], ['M','L'], [], 'ZZO11GQ', 200, 2,'regqerg',False, 0).work()
-
-# sample = self.driver.find_element_by_xpath("/html/body/div[2]/div/div/section/div[2]/div[1]/div/div/ul/li[" + str(i) + "]/span")
     def work(self):
         """
-        Starting bot job
-        :return:
-        """
+       Starting bot job
+       :return:
+       """
         self.driver.get("https://www.zalando-lounge.pl")
         self.perform_login()
         self.dont_know_whats_doing_this_part()
@@ -427,6 +419,10 @@ class ShoppingBot:
         self.scroll_down()
         self.iterate_over_items(self.get_filtered_hrefs(), self.sizes_list)
 
-
-ShoppingBot(['piotrpopisgames@gmail.com testertest', 'mtarka1337@gmail.com Azexs1998'], ['Koszula', 't-shirt'],
-            ['M', 'L'], [], 'ZZO11GQ', 300, 1, 'piotrpopis@icloud.com', True, 0).work()
+# ShoppingBot(['piotrpopisgames@gmail.com testertest', 'mtarka1337@gmail.com Azexs1998'], ['bluza'],
+#       ['M', 'L'], [], 'ZZO0ZBU', 300, 2, 'piotrpopis@icloud.com', True, 0).work()
+# ShoppingBot(["piotrpopisgames@gmail.com
+# testertest", "mtarka1337@gmail.com Azexs1998"], ['koszula'], ['M','L'
+# ], [], 'ZZO11GQ', 200, 2,'regqerg',False, 0).work()
+# sample = self.driver.find_element_by_
+# xpath("/html/body/div[2]/div/div/section/div[2]/div[1]/div/div/ul/li[" + str(i) + "]/span")
