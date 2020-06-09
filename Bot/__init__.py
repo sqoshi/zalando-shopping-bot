052e09b93ab541706342ca77b56d33abde939d82
Piotr <piotrpopis98@icloud.com>
2020-06-09 12:37:51 +0200

    doc + atomiziing

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -30,6 +30,11 @@ def sendMail(to, file):
 
 
 def check_size_availability(parent):
+    """
+    Checks if button is avaible to click.
+    :param parent:
+    :return:
+    """
     is_clickable = parent.value_of_css_property("color")
     if is_clickable == 'rgb(53, 53, 53)':
         return True
@@ -168,45 +173,24 @@ class ShoppingBot:
         try:
             WebDriverWait(self.driver, 3).until(
                 ec.presence_of_element_located((By.XPATH, '//div[contains(@class,"sizeOverlayDialog")]')))
-
             WebDriverWait(self.driver, 3).until(
                 ec.element_to_be_clickable((By.XPATH, '//span[text() = "Mimo to zamawiam oba rozmiary"]'))).click()
-
             WebDriverWait(self.driver, 3).until(
                 ec.element_to_be_clickable((By.XPATH, '//span[text() = "Potwierdź"]'))).click()
-
             return True
-
         except TimeoutException:
             action = ActionChains(self.driver)
-
             if attempt % 3 == 2:
-                self.driver.refresh()
-                button = WebDriverWait(self.driver, 3).until(
-                    ec.element_to_be_clickable((By.XPATH, '//*[@id="addToCartButton"]')))
-                cart = WebDriverWait(self.driver, 4).until(
-                    ec.element_to_be_clickable((By.XPATH, '//*[@id="header-cart"]')))
-                WebDriverWait(self.driver, 4).until(ec.element_to_be_clickable(
-                    (By.XPATH, '//span[contains(@class, "Size") and text()="' + size + '"]'))).click()
-                action.move_to_element(cart)
-                action.click(cart)
-                action.perform()
-                action.move_to_element(button)
-                action.click(button)
-                action.perform()
+                self.deal_with_bug(action, size)
                 return self.wait_for_popup(attempt + 1, size)
-
             else:
                 if attempt == 3:
                     return False
-
                 WebDriverWait(self.driver, 5).until(
                     ec.element_to_be_clickable((By.XPATH, '//*[@id="addToCartButton"]'))).click()
-
                 if self.driver.find_element_by_xpath(
                         '//*[@id="addToCartButton"]/div[1]/div[2]/span').text != 'Proszę wybrać rozmiar':
                     return False
-
                 return self.wait_for_popup(attempt + 1, size)
 
     def wait_for_popup_single(self):
@@ -217,19 +201,36 @@ class ShoppingBot:
         try:
             WebDriverWait(self.driver, 3).until(
                 ec.presence_of_element_located((By.XPATH, '//div[contains(@class,"sizeOverlayDialog")]')))
-
             WebDriverWait(self.driver, 3).until(
                 ec.element_to_be_clickable((By.XPATH, '//span[text() = "Mimo to zamawiam oba rozmiary"]'))).click()
-
             WebDriverWait(self.driver, 3).until(
                 ec.element_to_be_clickable((By.XPATH, '//span[text() = "Potwierdź"]'))).click()
-
             return True
-
         except TimeoutException:
             return False
 
-    def wait_for_atcButton(self, attempt, size, var):
+    def deal_with_bug(self, action, size):
+        """
+        Trying to click with action chain to deal with bug
+        :param action:
+        :param size:
+        :return:
+        """
+        self.driver.refresh()
+        button = WebDriverWait(self.driver, 3).until(
+            ec.element_to_be_clickable((By.XPATH, '//*[@id="addToCartButton"]')))
+        cart = WebDriverWait(self.driver, 4).until(
+            ec.element_to_be_clickable((By.XPATH, '//*[@id="header-cart"]')))
+        WebDriverWait(self.driver, 4).until(ec.element_to_be_clickable(
+            (By.XPATH, '//span[contains(@class, "Size") and text()="' + size + '"]'))).click()
+        action.move_to_element(cart)
+        action.click(cart)
+        action.perform()
+        action.move_to_element(button)
+        action.click(button)
+        action.perform()
+
+    def wait_acceptance_button(self, attempt, size, var):
         """
        Waiting till animation of adding item to shopping cart is finished or leaves error.
         :param var:
@@ -238,55 +239,32 @@ class ShoppingBot:
        :return:
        """
         try:
-
             WebDriverWait(self.driver, 3).until(ec.presence_of_element_located(
                 (By.XPATH, '//div[contains(@class, "animation-ball") and starts-with(@style, "transf") ]')))
             WebDriverWait(self.driver, 3).until(ec.presence_of_element_located(
                 (By.XPATH, '//div[contains(@class, "animation-ball") and starts-with(@style, "display: none;") ]')))
-
             return True
-
         except TimeoutException:
-
             print('atcBtn except:', attempt)
-
             action = ActionChains(self.driver)
-
             if attempt % 3 == 2:
-                self.driver.refresh()
-                button = WebDriverWait(self.driver, 3).until(
-                    ec.element_to_be_clickable((By.XPATH, '//*[@id="addToCartButton"]')))
-                cart = WebDriverWait(self.driver, 4).until(
-                    ec.element_to_be_clickable((By.XPATH, '//*[@id="header-cart"]')))
-                WebDriverWait(self.driver, 4).until(ec.element_to_be_clickable(
-                    (By.XPATH, '//span[contains(@class, "Size") and text()="' + size + '"]'))).click()
-                action.move_to_element(cart)
-                action.click(cart)
-                action.perform()
-                action.move_to_element(button)
-                action.click(button)
-                action.perform()
+                self.deal_with_bug(action, size)
                 if var:
                     if self.wait_for_popup_single():
                         return True
-                return self.wait_for_atcButton(attempt + 1, size, var)
-
+                return self.wait_acceptance_button(attempt + 1, size, var)
             else:
                 if attempt == 3:
                     return False
-
                 WebDriverWait(self.driver, 5).until(
                     ec.element_to_be_clickable((By.XPATH, '//*[@id="addToCartButton"]'))).click()
-
                 if var:
                     if self.wait_for_popup_single():
                         return True
-
                 if self.driver.find_element_by_xpath(
                         '//*[@id="addToCartButton"]/div[1]/div[2]/span').text != 'Proszę wybrać rozmiar':
                     return False
-
-                return self.wait_for_atcButton(attempt + 1, size, var)
+                return self.wait_acceptance_button(attempt + 1, size, var)
 
     def wait_login_error(self):
         """
@@ -298,15 +276,34 @@ class ShoppingBot:
             self.driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div/div/div/form/button').click()
             self.wait_login_error()
 
-    def change_acc(self, href, size):
+    def relog(self, href, size):
+        """
+        relog to next account
+        :param href:
+        :param size:
+        :return:
+        """
+        WebDriverWait(self.driver, 15).until(
+            ec.element_to_be_clickable((By.XPATH, '//span[contains(text(), "Zaloguj")]'))).click()
+        element = WebDriverWait(self.driver, 5).until(ec.element_to_be_clickable((By.XPATH, '//*[@id="form-email"]')))
+        cred = self.accounts_list[self.iteration].split()
+        element.send_keys(cred[0])
+        element = WebDriverWait(self.driver, 5).until(
+            ec.element_to_be_clickable((By.XPATH, '//*[@id="form-password"]')))
+        element.send_keys(cred[1])
+        element.submit()
+        self.wait_login_error()
+        self.driver.get(href)
+        WebDriverWait(self.driver, 5).until(ec.element_to_be_clickable(
+            (By.XPATH, '//span[contains(@class, "Size") and text()="' + size + '"]'))).click()
+
+    def change_account(self, href, size):
         """
         Changing account after reaching max shopping cart
         :param href:
         :param size:
         :return:
         """
-        print(self.iteration)
-        print(len(self.accounts_list))
         self.iteration += 1
         if self.iteration == len(self.accounts_list):
             return True
@@ -314,32 +311,13 @@ class ShoppingBot:
             ec.element_to_be_clickable((By.XPATH, '//span[text() = "Konto"]'))).click()
         WebDriverWait(self.driver, 5).until(
             ec.element_to_be_clickable((By.XPATH, '//span[contains(text(), "Wyloguj")]'))).click()
-
         while True:
             if len(self.driver.find_elements_by_xpath('//span[contains(text(), "Zaloguj")]')) == 0:
                 sleep(2)
             else:
                 break
         sleep(1)
-        WebDriverWait(self.driver, 15).until(
-            ec.element_to_be_clickable((By.XPATH, '//span[contains(text(), "Zaloguj")]'))).click()
-
-        element = WebDriverWait(self.driver, 5).until(ec.element_to_be_clickable((By.XPATH, '//*[@id="form-email"]')))
-        cred = self.accounts_list[self.iteration].split()
-        element.send_keys(cred[0])
-
-        element = WebDriverWait(self.driver, 5).until(
-            ec.element_to_be_clickable((By.XPATH, '//*[@id="form-password"]')))
-        element.send_keys(cred[1])
-
-        element.submit()
-
-        self.wait_login_error()
-
-        self.driver.get(href)
-
-        WebDriverWait(self.driver, 5).until(ec.element_to_be_clickable(
-            (By.XPATH, '//span[contains(@class, "Size") and text()="' + size + '"]'))).click()
+        self.relog(href, size)
         return False
 
     def filter_event(self):
@@ -375,7 +353,6 @@ class ShoppingBot:
        Logging to account, checks error, sends login,pwd
        :return:
        """
-        # Wait for cookies banner and close it
         while True:
             try:
                 WebDriverWait(self.driver, 5).until(
@@ -385,14 +362,11 @@ class ShoppingBot:
                 pass
         self.driver.find_element_by_xpath(
             "/html/body/div[2]/div/div[2]/div[1]/div/div/div[1]/div/div/div[2]/div/div/button").click()
-        # email
         element = WebDriverWait(self.driver, 25).until(ec.element_to_be_clickable((By.XPATH, '//*[@id="form-email"]')))
         element.send_keys(self.email)
-        # password
         element = WebDriverWait(self.driver, 20).until(
             ec.element_to_be_clickable((By.XPATH, '//*[@id="form-password"]')))
         element.send_keys(self.password)
-        # logging in
         element.submit()
         self.wait_login_error()
 
@@ -433,6 +407,11 @@ class ShoppingBot:
         second_campaign.click()
 
     def check_amount(self, parent):
+        """
+        Setup amount for late operations.
+        :param parent:
+        :return:
+        """
         amount = -1
         try:
             amount_span = parent.find_element_by_xpath('./span[2]')
@@ -442,6 +421,75 @@ class ShoppingBot:
         except NoSuchElementException:
             amount = self.max_ammount
         return amount
+
+    def should_send_mail(self):
+        """
+        Check if should sen mail
+        :return:
+        """
+        if self.inform_email is not None:
+            sendMail(self.inform_email, 'messages/normally_finished')
+
+    def break_barrier(self, size, total_items, cur_items):
+        """
+        Check condition wait acceptance
+        :param size:
+        :param total_items:
+        :param cur_items:
+        :return:
+        """
+        WebDriverWait(self.driver, 20).until(ec.invisibility_of_element_located(
+            (By.XPATH, '//div[contains(@class,"styles___backdrop")]')))
+        if self.wait_acceptance_button(0, size, True):
+            total_items, cur_items = total_items + 1, cur_items + 1
+        return total_items, cur_items
+
+    def click_add_button(self):
+        """
+        Trying to click addCCartButton
+        :return:
+        """
+        button = WebDriverWait(self.driver, 5).until(
+            ec.element_to_be_clickable((By.XPATH, '//*[@id="addToCartButton"]')))
+        button.click()
+
+    def iterate_amount(self, amount, selected, size, total_items, href, size_shopping_cart=5):
+        """
+        operated on opened href - checking bugs,adding amount times "same" item
+        :param amount:
+        :param selected:
+        :param size:
+        :param total_items:
+        :param href:
+        :param size_shopping_cart:
+        :return:
+        """
+        for x in range(int(amount)):
+            cur_items = 0
+            self.click_add_button()
+
+            if selected == 2 and x == 0:
+                if self.wait_for_popup(0, size) is True:
+                    total_items, cur_items = self.break_barrier(size, total_items, cur_items)
+                else:
+                    break
+            else:
+                if self.wait_acceptance_button(0, size, False) is True:
+                    total_items, cur_items = total_items + 1, cur_items + 1
+                else:
+                    if x + 1 == amount and cur_items == 0 and selected == 1:
+                        selected = 0
+                    break
+            if total_items == size_shopping_cart:
+                total_items = 0
+                if x + 1 == int(amount):
+                    selected = 0
+                if self.change_account(href, size):
+                    self.should_send_mail()
+                    return amount, selected, size, total_items, True
+            if x + 1 == amount and cur_items == 0:
+                selected -= 1
+        return amount, selected, size, total_items, False
 
     def iterate_over_items(self, hrefs, selected_sizes, total_items=0):
         """
@@ -453,76 +501,36 @@ class ShoppingBot:
        """
         for href in hrefs:
             self.driver.get(href)
-
             WebDriverWait(self.driver, 5).until(
                 ec.presence_of_element_located((By.XPATH, "//div[starts-with(@class, 'ArticleSizestyles')]")))
-
             selected = 0
-
             if self.driver.find_element_by_xpath(
                     '//*[@id="addToCartButton"]/div[1]/div[2]/span').text != 'Proszę wybrać rozmiar':
                 continue
-
             for size in selected_sizes:
                 if selected == 2:
                     break
-
                 try:
                     element = WebDriverWait(self.driver, 3).until(ec.element_to_be_clickable(
                         (By.XPATH, '//span[contains(@class, "Size") and text()="' + size + '"]')))
                 except TimeoutException:
                     continue
-
                 parent = element.find_element_by_xpath("./..")
                 if check_size_availability(parent):
-
                     amount = self.check_amount(parent)
-
                     element.click()
                     selected = selected + 1
-
-                    for x in range(int(amount)):
-
-                        cur_items = 0
-                        button = WebDriverWait(self.driver, 5).until(
-                            ec.element_to_be_clickable((By.XPATH, '//*[@id="addToCartButton"]')))
-                        button.click()
-
-                        if selected == 2 and x == 0:
-                            if self.wait_for_popup(0, size) is True:
-                                WebDriverWait(self.driver, 20).until(ec.invisibility_of_element_located(
-                                    (By.XPATH, '//div[contains(@class,"styles___backdrop")]')))
-                                if self.wait_for_atcButton(0, size, True):
-                                    total_items += 1
-                                    cur_items += 1
-                            else:
-                                break
-                        else:
-                            if self.wait_for_atcButton(0, size, False) is True:
-                                total_items += 1
-                                cur_items += 1
-                            else:
-                                if x + 1 == amount and cur_items == 0 and selected == 1:
-                                    selected = 0
-                                break
-                        if total_items == 5:
-                            total_items = 0
-                            if x + 1 == int(amount):
-                                selected = 0
-                            if self.change_acc(href, size):
-                                if self.inform_email is not None:
-                                    sendMail(self.inform_email, '../messages/normally_finished')
-                                return True
-
-                        if x + 1 == amount and cur_items == 0:  # and selected ==1
-                            selected -= 1
-        if self.inform_email is not None:
-            sendMail(self.inform_email, 'messages/normally_finished')
+                    amount, selected, size, total_items, flag = self.iterate_amount(amount, selected, size, total_items,
+                                                                                    href)
+                    if flag:
+                        return True
+        self.should_send_mail()
         return True
 
     def work(self):
         """
        Starting bot job
+       //main loop
        :return:
        """
         self.driver.get("https://www.zalando-lounge.pl")
